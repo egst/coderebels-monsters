57e51c8e231cea670c03e7820a9d366ec227c72d
Maximilian Kulikov <maximilian.kulikov@egst.dev>
2024-04-22 18:16:55 +0200

    cleanup

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-const getData = () =>
+const getUsers = () =>
     new Promise((resolve) => {
         const fetchPromise = fetch('https://jsonplaceholder.typicode.com/users')
 
         fetchPromise.then(fetchResult => {
             const jsonPromise = fetchResult.json()
             jsonPromise.then(jsonResult => {
-                console.log(jsonResult)
                 resolve(jsonResult) // "return jsonResult"
             })
         })
     })
 
-getData().then(data => {
-    console.log(data)
-})
-
 document.addEventListener('DOMContentLoaded', () => {
     const list = document.querySelector('#list')
-    console.log(list)
-    
-    const name = 'Otakar Pupka'
-    const img  = 'john-doe.png'
-    const mail = 'pupka@example.com'
-    list.innerHTML = `
-        <div>
-            <img src="${img}" alt="User Photo">
-            <h2>${name}</h2>
-            <a href="mailto:${mail}">
-                ${mail}
-            </a>
-        </div>
-    `
+
+    getUsers().then(users => {
+        for (const user of users) {
+            list.innerHTML += `
+                <div>
+                    <img src="${null}" alt="User Photo">
+                    <h2>${user.name}</h2>
+                    <a href="mailto:${user.email}">
+                        ${user.email}
+                    </a>
+                </div>
+            `
+        }
+    })
 })
