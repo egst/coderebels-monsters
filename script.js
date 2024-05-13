8eba0a9a9e3a48fdbf079c9d196ef1b8e44bd74c
Maximilian Kulikov <maximilian.kulikov@egst.dev>
2024-05-13 19:32:08 +0200

    search

--- script.js
+++ script.js
@@ -1,36 +1,57 @@
-/*
-const getUsers = () =>
-    new Promise((resolve) => {
-        const fetchPromise = fetch('https://jsonplaceholder.typicode.com/users')
-        fetchPromise.then(fetchResult => {
-            const jsonPromise = fetchResult.json()
-            jsonPromise.then(jsonResult => {
-                resolve(jsonResult) // "return jsonResult"
-            })
-        })
-    })
-*/
-
-// syntax sugar
-// async a await
-// async - asynchronous "asynchronní"
-
 const getUsers = async () => {
     const fetchResult = await fetch('https://jsonplaceholder.typicode.com/users')
     return await fetchResult.json()
 }
 
-// TODO: Search.
-// GPT hint: https://chat.openai.com/share/f3ed1b48-f208-4b0c-8d08-45c7b5c9c9b5
+// Vsuvka: terminologie parametry vs argumenty
+//
+// Parametry - pri definici funkce:
+// f = (a, b, c) => { a... }
+//
+// Argumenty - pri volani:
+// f(1, 2, 3)
 
-document.addEventListener('DOMContentLoaded', async () => {
-    const list = document.querySelector('#list')
-
-    const users = await getUsers()
-
+const renderUsers = (list, users, search) => {
+    list.innerHTML = ''
     for (const user of users) {
+        // Napoveda:
+        // text1.includes(text2) - text1 obsahuje text2 (napr. 'foobar' obsahuje 'oob')
+        // user.name             - uzivatelske jmeno
+        // search                - vyhledavany text
+        // text.toLowerCase()    - prevest vse na mala pismena (napr. 'FooBar' -> 'foobar')
+        //
+        // !  - not (negace)
+        // || - or  (nebo)
+
+        // Level 1:
+        //if (user.name == search) {
+
+        // Level 2:
+        //if (user.name.includes(search)) {
+
+        // Level 3:
+        if (!search || user.name.toLowerCase().includes(search.toLowerCase())) {
+            const imageUrl = `https://robohash.org/${user.id}?set=set2`
+
+            list.innerHTML += `
+                <div>
+                    <img src="${imageUrl}" alt="User Photo">
+                    <h2>${user.name}</h2>
+                    <a href="mailto:${user.email}">
+                        ${user.email}
+                    </a>
+                </div>
+            `
+        }
+
+        // Alternativni zapis:
+
+        /*
+        if (!...) {
+            continue
+        }
+
         const imageUrl = `https://robohash.org/${user.id}?set=set2`
-        //const imageUrl = `/pictures/${user.id}.png`
 
         list.innerHTML += `
             <div>
@@ -41,5 +62,20 @@ document.addEventListener('DOMContentLoaded', async () => {
                 </a>
             </div>
         `
+        */
     }
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const list = document.querySelector('#list')
+
+    const users = await getUsers()
+
+    // Sem?
+    renderUsers(list, users)
+
+    const searchInput = document.querySelector('#search')
+    searchInput.addEventListener('input', () => {
+        renderUsers(list, users, searchInput.value)
+    })
 })
